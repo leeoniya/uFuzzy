114b6aa3e273a214c733b3311559fc57a5ad1425
Leon Sorokin <leeoniya@gmail.com>
2022-09-15 23:50:20 -0500

    a commitment

---
+++ dist/uFuzzy.cjs.js
@@ -0,0 +1,223 @@
+/**
+* Copyright (c) 2022, Leon Sorokin
+* All rights reserved. (MIT Licensed)
+*
+* uFuzzy.js (μFuzzy)
+* A fuzzy matcher that doesn't suck
+* https://github.com/leeoniya/uFuzzy (v0.0.1)
+*/
+
+'use strict';
+
+const cmp = new Intl.Collator('en').compare;
+
+const inf = Infinity;
+
+const OPTS = {
+	// term segmentation & punct/whitespace merging
+	interSplit: '[^A-Za-z0-9]+',
+	intraSplit: '[A-Za-z][0-9]|[0-9][A-Za-z]|[a-z][A-Z]',
+
+	// allowance between terms
+	interChars: '.',
+	interLimit: inf,
+
+	// allowance between chars in terms
+	intraChars: '[\\w-]',
+	intraLimit: 0,
+
+	// max filtered matches before scoring kicks in
+//	scoreLimit: 1000,
+	// should scoring compute matched substr ranges for highlighting
+	withRanges: false,
+
+	// final sorting fn
+	sort: (scored, haystack, needle) => {
+		let ranked = scored.slice().sort((a, b) => (
+			   b.eSyms - a.eSyms
+			|| b.eBnds - a.eBnds
+			|| a.span - b.span
+			|| a.start - b.start
+			|| cmp(haystack[a.idx], haystack[b.idx])
+	 	));
+
+		return ranked;
+	},
+};
+
+const lazyRepeat = (chars, limit) => (
+	limit == 0   ? ''           :
+	limit == 1   ? chars + '??' :
+	limit == inf ? chars + '*?' :
+	               chars + `{0,${limit}}?`
+);
+
+function uFuzzy(opts) {
+	opts = Object.assign({}, OPTS, opts);
+
+	let intraSplit = new RegExp(opts.intraSplit, 'g');
+	let interSplit = new RegExp(opts.interSplit, 'g');
+
+	const prepQuery = (query, capt = 0) => {
+		// split on punct, whitespace, num-alpha, and upper-lower boundaries
+		let parts = query.trim().replace(intraSplit, m => m[0] + ' ' + m[1]).split(interSplit);
+
+		let intraCharsTpl = lazyRepeat(opts.intraChars, opts.intraLimit);
+
+		// capture at char level
+		if (capt == 2 && opts.intraLimit > 0) {
+			// sadly, we also have to capture the inter-term junk via parenth-wrapping .*?
+			// to accum other capture groups' indices for \b boosting during scoring
+			intraCharsTpl = ')(' + intraCharsTpl + ')(';
+		}
+
+		let reTpl = parts.map(p => p.split('').join(intraCharsTpl));
+
+		let interCharsTpl = lazyRepeat(opts.interChars, opts.interLimit);
+
+		// capture at word level
+		if (capt > 0) {
+			// sadly, we also have to capture the inter-term junk via parenth-wrapping .*?
+			// to accum other capture groups' indices for \b boosting during scoring
+			reTpl = '(' + reTpl.join(')(' + interCharsTpl + ')(') + ')';
+		}
+		else
+			reTpl = reTpl.join(interCharsTpl);
+
+		return [new RegExp(reTpl, 'i'), parts];
+	};
+
+	const filter = (haystack, needle) => {
+
+		let out = [];
+		let [query] = prepQuery(needle);
+
+		for (let i = 0; i < haystack.length; i++) {
+			let item = haystack[i];
+			query.test(item) && out.push(i);
+		}
+
+		return out;
+	};
+
+	let nonAciiRe = /[^a-z0-9]/i;
+	let intraBound = new RegExp(opts.intraSplit);
+
+	const rank = (idxs, haystack, needle) => {
+
+		let [query, parts] = prepQuery(needle, 1);
+
+		let queryR;
+
+		if (opts.withRanges)
+			[queryR] = prepQuery(needle, 2);
+
+		let scored = Array(idxs.length);
+
+		for (let i = 0; i < idxs.length; i++) {
+			let mhstr = haystack[idxs[i]];
+			let m = mhstr.match(query);
+
+			let eSyms = 0;	// symbols matched in exact terms matched
+			let eBnds = 0;	// bounds of matched groups
+			let idxAcc = m.index;
+			let span = m[0].length;
+
+			for (let j = 0, k = 1; j < parts.length; j++, k+=2) {
+				let group = m[k].toLowerCase();
+				let fullMatch = group == parts[j];
+
+				// when intraLimit > 0 'test' query can match 'ttest' in 'fittest'
+				// try an exact substring match to improve rank quality
+				if (opts.intraLimit > 0 && !fullMatch) {
+					let idxOf = group.indexOf(parts[j]);
+					if (idxOf > -1) {
+						fullMatch = true;
+						idxAcc += idxOf;
+						m[k] = m[k].slice(idxOf);
+
+						if (j == 0) {
+							m.index = idxAcc;
+							span -= idxOf;
+						}
+					}
+
+					// TODO: use difference in group/part length to boost eSyms? or iSyms (inexact)
+				}
+
+				if (fullMatch) {
+					eSyms += parts[j].length;
+
+					// does group's left and/or right land on \b
+					let lftCharIdx = idxAcc - 1;
+					let rgtCharIdx = idxAcc + m[k].length;
+
+					let lftBoost = 0;
+					let rgtBoost = 0;
+
+					// prefix boosts
+					if (lftCharIdx == -1           || nonAciiRe.test(mhstr[lftCharIdx]))
+						lftBoost += 1;
+					else if (intraBound.test(mhstr[lftCharIdx] + mhstr[lftCharIdx + 1]))
+						lftBoost += 0.5;
+
+					// suffix boosts
+					if (rgtCharIdx == mhstr.length || nonAciiRe.test(mhstr[rgtCharIdx]))
+						rgtBoost += 1;
+					else if (intraBound.test(mhstr[rgtCharIdx - 1] + mhstr[rgtCharIdx]))
+						rgtBoost += 0.5;
+
+					eBnds = lftBoost + rgtBoost + (lftBoost > 0 && rgtBoost > 0 ? 1 : 0);
+				}
+
+				if (j < parts.length - 1)
+					idxAcc += m[k].length + m[k+1].length;
+			}
+
+			let match  = {
+				eSyms,
+				eBnds,
+				span,
+				start: m.index,
+				idx: idxs[i],
+			};
+
+			if (opts.withRanges) {
+				let m = mhstr.match(queryR);
+				let ranges = match.ranges = [];
+
+				let idxAcc = m.index;
+				let from = idxAcc;
+				let to = idxAcc;
+				for (let i = 1; i < m.length; i++) {
+					let len = m[i].length;
+
+					idxAcc += len;
+
+					if (i % 2)
+						to = idxAcc;
+					else if (len > 0) {
+						ranges.push(from, to);
+						from = to = idxAcc;
+					}
+				}
+
+				if (to > from)
+					ranges.push(from, to);
+			}
+
+			scored[i] = match;
+		}
+
+		let ranked = opts.sort(scored, haystack, needle);
+
+		return ranked;
+	};
+
+	return {
+		filter,
+		rank,
+	};
+}
+
+module.exports = uFuzzy;
---
+++ rollup.config.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+
+import { terser } from 'rollup-plugin-terser';
+
+const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+const ver = "v" + pkg.version;
+const urlVer = "https://github.com/leeoniya/uFuzzy (" + ver + ")";
+const banner = [
+	"/**",
+	"* Copyright (c) " + new Date().getFullYear() + ", Leon Sorokin",
+	"* All rights reserved. (MIT Licensed)",
+	"*",
+	"* uFuzzy.js (μFuzzy)",
+	"* A fuzzy matcher that doesn't suck",
+	"* " + urlVer,
+	"*/",
+	"",
+].join("\n");
+
+function bannerlessESM() {
+	return {
+		name: 'stripBanner',
+		resolveId(importee) {
+			if (importee == 'uFuzzy')
+				return importee;
+			return null;
+		},
+		load(id) {
+			if (id == 'uFuzzy')
+				return fs.readFileSync('./dist/uFuzzy.esm.js', 'utf8').replace(/\/\*\*.*?\*\//gms, '');
+			return null;
+		}
+	};
+}
+
+const terserOpts = {
+	compress: {
+		inline: 0,
+		passes: 2,
+		keep_fargs: false,
+		pure_getters: true,
+		unsafe: true,
+		unsafe_comps: true,
+		unsafe_math: true,
+		unsafe_undefined: true,
+	},
+	output: {
+		comments: /^!/
+	}
+};
+
+export default [
+	{
+		input: './src/uFuzzy.js',
+		output: {
+			name: 'uFuzzy',
+			file: './dist/uFuzzy.esm.js',
+			format: 'es',
+			banner,
+		},
+	},
+	{
+		input: './src/uFuzzy.js',
+		output: {
+			name: 'uFuzzy',
+			file: './dist/uFuzzy.cjs.js',
+			format: 'cjs',
+			exports: "auto",
+			banner,
+		},
+	},
+	{
+		input: 'uFuzzy',
+		output: {
+			name: 'uFuzzy',
+			file: './dist/uFuzzy.iife.js',
+			format: 'iife',
+			esModule: false,
+			banner,
+		},
+		plugins: [
+			bannerlessESM(),
+		]
+	},
+	{
+		input: 'uFuzzy',
+		output: {
+			name: 'uFuzzy',
+			file: './dist/uFuzzy.iife.min.js',
+			format: 'iife',
+			esModule: false,
+			banner: "/*! " + urlVer + " */",
+		},
+		plugins: [
+			bannerlessESM(),
+			terser(terserOpts),
+		]
+	},
+];
