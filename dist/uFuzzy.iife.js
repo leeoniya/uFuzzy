ba63d3f1249c17d0663b7fe0862613bde724e734
Leon Sorokin <leeoniya@gmail.com>
2022-09-30 00:34:23 -0500

    0.6.0

--- dist/uFuzzy.iife.js
+++ dist/uFuzzy.iife.js
@@ -4,7 +4,7 @@
 *
 * uFuzzy.js (μFuzzy)
 * A fuzzy matcher that doesn't suck
-* https://github.com/leeoniya/uFuzzy (v0.5.1)
+* https://github.com/leeoniya/uFuzzy (v0.6.0)
 */
 
 var uFuzzy = (function () {
