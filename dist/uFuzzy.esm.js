b348d5bbd94af1337e11f167fde39086d58e12f4
Leon Sorokin <leeoniya@gmail.com>
2023-02-24 19:34:52 -0600

    1.0.3

--- dist/uFuzzy.esm.js
+++ dist/uFuzzy.esm.js
@@ -4,7 +4,7 @@
 *
 * uFuzzy.js (μFuzzy)
 * A tiny, efficient fuzzy matcher that doesn't suck
-* https://github.com/leeoniya/uFuzzy (v1.0.2)
+* https://github.com/leeoniya/uFuzzy (v1.0.3)
 */
 
 const cmp = new Intl.Collator('en').compare;
