7724cd0b41d4cd613f08374c876793bff81b3297
Leon Sorokin <leeoniya@gmail.com>
2024-11-19 18:58:18 -0600

    1.0.15

--- dist/uFuzzy.cjs.js
+++ dist/uFuzzy.cjs.js
@@ -4,7 +4,7 @@
 *
 * uFuzzy.js (μFuzzy)
 * A tiny, efficient fuzzy matcher that doesn't suck
-* https://github.com/leeoniya/uFuzzy (v1.0.14)
+* https://github.com/leeoniya/uFuzzy (v1.0.15)
 */
 
 'use strict';
