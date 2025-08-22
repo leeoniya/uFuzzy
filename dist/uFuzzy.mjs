4d89651ea5acba2f448df58d7c4f6165c7474924
Leon Sorokin <leeoniya@gmail.com>
2025-08-21 22:15:02 -0500

    1.0.19

--- dist/uFuzzy.mjs
+++ dist/uFuzzy.mjs
@@ -4,7 +4,7 @@
 *
 * uFuzzy.js (μFuzzy)
 * A tiny, efficient fuzzy matcher that doesn't suck
-* https://github.com/leeoniya/uFuzzy (v1.0.18)
+* https://github.com/leeoniya/uFuzzy (v1.0.19)
 */
 
 const cmp = (a, b) => a > b ? 1 : a < b ? -1 : 0;
